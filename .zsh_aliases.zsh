bf6ba7aef018c5d5a86c10c49437c89261ac67f7
tsa96 <tom@trumpetdust.org>
2023-07-21 06:03:05 +0100

    initial

---
+++ .zsh_aliases.zsh
@@ -0,0 +1,380 @@
+# Bunch of shit from oh-my-zsh. I don't want to 
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+alias -- -='cd -'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
+alias md='mkdir -p'
+alias rd=rmdir
+
+function d () {
+  if [[ -n $1 ]]; then
+    dirs "$@"
+  else
+    dirs -v | head -n 10
+  fi
+}
+compdef _dirs d
+
+# List directory contents
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+alias-finder() {
+  local cmd="" exact="" longer="" wordStart="" wordEnd="" multiWordEnd=""
+  for i in $@; do
+    case $i in
+      -e|--exact) exact=true;;
+      -l|--longer) longer=true;;
+      *)
+        if [[ -z $cmd ]]; then
+          cmd=$i
+        else
+          cmd="$cmd $i"
+        fi
+        ;;
+    esac
+  done
+  cmd=$(sed 's/[].\|$(){}?+*^[]/\\&/g' <<< $cmd) # adds escaping for grep
+  if (( $(wc -l <<< $cmd) == 1 )); then
+    while [[ $cmd != "" ]]; do
+      if [[ $longer = true ]]; then
+        wordStart="'{0,1}"
+      else
+        wordEnd="$"
+        multiWordEnd="'$"
+      fi
+      if [[ $cmd == *" "* ]]; then
+        local finder="'$cmd$multiWordEnd"
+      else
+        local finder=$wordStart$cmd$wordEnd
+      fi
+      alias | grep -E "=$finder"
+      if [[ $exact = true || $longer = true ]]; then
+        break
+      else
+        cmd=$(sed -E 's/ {0,1}[^ ]*$//' <<< $cmd) # removes last word
+      fi
+    done
+  fi
+}
+
+preexec_alias-finder() {
+  if [[ $ZSH_ALIAS_FINDER_AUTOMATIC = true ]]; then
+    alias-finder $1
+  fi
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook preexec preexec_alias-finder
+
+function gcnv() {
+	if [ -z "$1" ]; then
+		echo 'no arg provided. fuck yourself'
+	else
+		git commit --no-verify -m "$*"
+	fi
+}
+
+function gcdnv() {
+	if [ -z "$2" ]; then
+		echo 'not enough args provided. fuck yourself'
+	else
+		git commit --no-verify -m "$1" -m "$2"
+	fi
+}
+
+# alias '-'='cd -'
+# alias '...'='../..'
+# alias '....'='../../..'
+# alias '.....'='../../../..'
+# alias '......'='../../../../..'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+alias _='sudo '
+alias dbl='docker build'
+alias dcb='docker-compose build'
+alias dcdn='docker-compose down'
+alias dce='docker-compose exec'
+alias dcin='docker container inspect'
+alias dck='docker-compose kill'
+alias dcl='docker-compose logs'
+alias dclf='docker-compose logs -f'
+alias dcls='docker container ls'
+alias dclsa='docker container ls -a'
+alias dco=docker-compose
+alias dcps='docker-compose ps'
+alias dcpull='docker-compose pull'
+alias dcr='docker-compose run'
+alias dcrestart='docker-compose restart'
+alias dcrm='docker-compose rm'
+alias dcstart='docker-compose start'
+alias dcstop='docker-compose stop'
+alias dcup='docker-compose up'
+alias dcupb='docker-compose up --build'
+alias dcupd='docker-compose up -d'
+alias dcupdb='docker-compose up -d --build'
+alias dib='docker image build'
+alias dii='docker image inspect'
+alias dils='docker image ls'
+alias dipu='docker image push'
+alias dirm='docker image rm'
+alias dit='docker image tag'
+alias dlo='docker container logs'
+alias dnc='docker network create'
+alias dncn='docker network connect'
+alias dndcn='docker network disconnect'
+alias dni='docker network inspect'
+alias dnls='docker network ls'
+alias dnrm='docker network rm'
+alias dpo='docker container port'
+alias dpu='docker pull'
+alias dr='docker container run'
+alias drit='docker container run -it'
+alias drm='docker container rm'
+alias 'drm!'='docker container rm -f'
+alias drs='docker container restart'
+alias dst='docker container start'
+alias dsta='docker stop $(docker ps -q)'
+alias dstp='docker container stop'
+alias dtop='docker top'
+alias dvi='docker volume inspect'
+alias dvls='docker volume ls'
+alias dvprune='docker volume prune'
+alias dxc='docker container exec'
+alias dxcit='docker container exec -it'
+alias egrep='grep -E --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
+alias fgrep='grep -F --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
+alias g=git
+alias ga='git add'
+alias gaa='git add --all'
+alias gam='git am'
+alias gama='git am --abort'
+alias gamc='git am --continue'
+alias gams='git am --skip'
+alias gamscp='git am --show-current-patch'
+alias gap='git apply'
+alias gapa='git add --patch'
+alias gapt='git apply --3way'
+alias gau='git add --update'
+alias gav='git add --verbose'
+alias gb='git branch'
+alias gbD='git branch --delete --force'
+alias gba='git branch --all'
+alias gbd='git branch --delete'
+alias gbda='git branch --no-color --merged | command grep -vE "^([+*]|\s*($(git_main_branch)|$(git_develop_branch))\s*$)" | command xargs git branch --delete 2>/dev/null'
+alias gbg='git branch -vv | grep ": gone\]"'
+alias gbgD='git branch --no-color -vv | grep ": gone\]" | awk '\''{print $1}'\'' | xargs git branch -D'
+alias gbgd='git branch --no-color -vv | grep ": gone\]" | awk '\''{print $1}'\'' | xargs git branch -d'
+alias gbl='git blame -b -w'
+alias gbnm='git branch --no-merged'
+alias gbr='git branch --remote'
+alias gbs='git bisect'
+alias gbsb='git bisect bad'
+alias gbsg='git bisect good'
+alias gbsr='git bisect reset'
+alias gbss='git bisect start'
+alias gc='git commit --verbose'
+alias 'gc!'='git commit --verbose --amend'
+alias gca='git commit --verbose --all'
+alias 'gca!'='git commit --verbose --all --amend'
+alias gcam='git commit --all --message'
+alias 'gcan!'='git commit --verbose --all --no-edit --amend'
+alias 'gcans!'='git commit --verbose --all --signoff --no-edit --amend'
+alias gcas='git commit --all --signoff'
+alias gcasm='git commit --all --signoff --message'
+alias gcb='git checkout -b'
+alias gcd='git checkout $(git_develop_branch)'
+alias gcf='git config --list'
+alias gcl='git clone --recurse-submodules'
+alias gclean='git clean --interactive -d'
+alias gcm='git checkout $(git_main_branch)'
+alias gcmsg='git commit --message'
+alias 'gcn!'='git commit --verbose --no-edit --amend'
+alias gco='git checkout'
+alias gcor='git checkout --recurse-submodules'
+alias gcount='git shortlog --summary --numbered'
+alias gcp='git cherry-pick'
+alias gcpa='git cherry-pick --abort'
+alias gcpc='git cherry-pick --continue'
+alias gcs='git commit --gpg-sign'
+alias gcsm='git commit --signoff --message'
+alias gcss='git commit --gpg-sign --signoff'
+alias gcssm='git commit --gpg-sign --signoff --message'
+alias gd='git diff'
+alias gdca='git diff --cached'
+alias gdct='git describe --tags $(git rev-list --tags --max-count=1)'
+alias gdcw='git diff --cached --word-diff'
+alias gds='git diff --staged'
+alias gdt='git diff-tree --no-commit-id --name-only -r'
+alias gdup='git diff @{upstream}'
+alias gdw='git diff --word-diff'
+alias gf='git fetch'
+alias gfa='git fetch --all --prune --jobs=10'
+alias gfg='git ls-files | grep'
+alias gfo='git fetch origin'
+alias gg='git gui citool'
+alias gga='git gui citool --amend'
+alias ggpull='git pull origin "$(git_current_branch)"'
+alias ggpur=ggu
+alias ggpush='git push origin "$(git_current_branch)"'
+alias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'
+alias ghh='git help'
+alias gignore='git update-index --assume-unchanged'
+alias gignored='git ls-files -v | grep "^[[:lower:]]"'
+alias git-svn-dcommit-push='git svn dcommit && git push github $(git_main_branch):svntrunk'
+alias gk='\gitk --all --branches &!'
+alias gke='\gitk --all $(git log --walk-reflogs --pretty=%h) &!'
+alias gl='git pull'
+alias glg='git log --stat'
+alias glgg='git log --graph'
+alias glgga='git log --graph --decorate --all'
+alias glgm='git log --graph --max-count=10'
+alias glgp='git log --stat --patch'
+alias glo='git log --oneline --decorate'
+alias globurl='noglob urlglobber '
+alias glod='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'\'
+alias glods='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'\'' --date=short'
+alias glog='git log --oneline --decorate --graph'
+alias gloga='git log --oneline --decorate --graph --all'
+alias glol='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'\'
+alias glola='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'\'' --all'
+alias glols='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'\'' --stat'
+alias glp=_git_log_prettily
+alias gluc='git pull upstream $(git_current_branch)'
+alias glum='git pull upstream $(git_main_branch)'
+alias gm='git merge'
+alias gma='git merge --abort'
+alias gmom='git merge origin/$(git_main_branch)'
+alias gms='git merge --squash'
+alias gmtl='git mergetool --no-prompt'
+alias gmtlvim='git mergetool --no-prompt --tool=vimdiff'
+alias gmum='git merge upstream/$(git_main_branch)'
+alias gp='git push'
+alias gpd='git push --dry-run'
+alias gpf='git push --force-with-lease --force-if-includes'
+alias 'gpf!'='git push --force'
+alias gpoat='git push origin --all && git push origin --tags'
+alias gpod='git push origin --delete'
+alias gpr='git pull --rebase'
+alias gpristine='git reset --hard && git clean --force -dfx'
+alias gpsup='git push --set-upstream origin $(git_current_branch)'
+alias gpsupf='git push --set-upstream origin $(git_current_branch) --force-with-lease --force-if-includes'
+alias gpu='git push upstream'
+alias gpv='git push --verbose'
+alias gr='git remote'
+alias gra='git remote add'
+alias grb='git rebase'
+alias grba='git rebase --abort'
+alias grbc='git rebase --continue'
+alias grbd='git rebase $(git_develop_branch)'
+alias grbi='git rebase --interactive'
+alias grbm='git rebase $(git_main_branch)'
+alias grbo='git rebase --onto'
+alias grbom='git rebase origin/$(git_main_branch)'
+alias grbs='git rebase --skip'
+alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
+alias grev='git revert'
+alias grh='git reset'
+alias grhh='git reset --hard'
+alias grm='git rm'
+alias grmc='git rm --cached'
+alias grmv='git remote rename'
+alias groh='git reset origin/$(git_current_branch) --hard'
+alias grrm='git remote remove'
+alias grs='git restore'
+alias grset='git remote set-url'
+alias grss='git restore --source'
+alias grst='git restore --staged'
+alias grt='cd "$(git rev-parse --show-toplevel || echo .)"'
+alias gru='git reset --'
+alias grup='git remote update'
+alias grv='git remote --verbose'
+alias gsb='git status --short --branch'
+alias gsd='git svn dcommit'
+alias gsh='git show'
+alias gsi='git submodule init'
+alias gsps='git show --pretty=short --show-signature'
+alias gsr='git svn rebase'
+alias gss='git status --short'
+alias gst='git status'
+alias gsta='git stash push'
+alias gstaa='git stash apply'
+alias gstall='git stash --all'
+alias gstc='git stash clear'
+alias gstd='git stash drop'
+alias gstl='git stash list'
+alias gstp='git stash pop'
+alias gsts='git stash show --text'
+alias gstu='gsta --include-untracked'
+alias gsu='git submodule update'
+alias gsw='git switch'
+alias gswc='git switch --create'
+alias gswd='git switch $(git_develop_branch)'
+alias gswm='git switch $(git_main_branch)'
+alias gtl='gtl(){ git tag --sort=-v:refname -n --list "${1}*" }; noglob gtl'
+alias gts='git tag --sign'
+alias gtv='git tag | sort -V'
+alias gunignore='git update-index --no-assume-unchanged'
+alias gunwip='git rev-list --max-count=1 --format="%s" HEAD | grep -q "\--wip--" && git reset HEAD~1'
+alias gup='git pull --rebase'
+alias gupa='git pull --rebase --autostash'
+alias gupav='git pull --rebase --autostash --verbose'
+alias gupom='git pull --rebase origin $(git_main_branch)'
+alias gupomi='git pull --rebase=interactive origin $(git_main_branch)'
+alias gupv='git pull --rebase --verbose'
+alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign --message "--wip-- [skip ci]"'
+alias gwt='git worktree'
+alias gwta='git worktree add'
+alias gwtls='git worktree list'
+alias gwtmv='git worktree move'
+alias gwtrm='git worktree remove'
+alias history='omz_history -t '\''dd/mm/yy'\'
+alias l='ls -lah'
+alias la='ls -lAh'
+alias ll='ls -lh'
+alias ls='ls --color=tty'
+alias lsa='ls -lah'
+alias md='mkdir -p'
+alias mom1='ssh mombot@198.46.215.178'
+alias nxfa='nx format --all'
+alias nxmany='nx run-many -t'
+alias nxr='nx reset'
+alias nxs='nx serve'
+alias oc1='ssh.exe ubuntu@144.21.52.121'
+alias rd=rmdir
+alias run-help=man
+alias ta='tmux attach -t'
+alias tad='tmux attach -d -t'
+alias tkss='tmux kill-session -t'
+alias tksv='tmux kill-server'
+alias tl='tmux list-sessions'
+alias tmux=_zsh_tmux_plugin_run
+alias tmuxconf='$EDITOR $ZSH_TMUX_CONFIG'
+alias ts='tmux new-session -s'
+alias which-command=whence
+alias nxs="nx serve"
+alias nxfa="nx format --all"
+alias nxmany="nx run-many -t"
+alias nxr="nx reset"
---
+++ .zshrc
@@ -0,0 +1,30 @@
+# This setup https://www.atlassian.com/git/tutorials/dotfiles
+alias config='git --git-dir=/c/Users/tom/.cfg/ --work-tree=/c/Users/tom'
+
+# Horrid hack to be able to use WT with -here
+if [ "$(pwd)" = '/c/Windows/System32' ]; then
+	cd
+fi
+
+# https://unix.stackexchange.com/questions/58870/ctrl-left-right-arrow-keys-issue
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Aliases
+source ~/.zsh_aliases.zsh
+source ~/.zsh_aliases_private.zsh
+
+# Theme - p10k submoduled
+source ~/powerlevel10k/powerlevel10k.zsh-theme
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# Completion
+autoload -Uz compinit && compinit
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'alias config='git --git-dir=/c/Users/tom/.cfg/ --work-tree=/c/Users/tom'
