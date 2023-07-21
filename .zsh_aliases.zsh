96010a89090e1fb41993acaaa6cdbd91ca640480
tsa96 <tom@trumpetdust.org>
2023-07-21 06:08:23 +0100

    cleanup

--- .zsh_aliases.zsh
+++ .zsh_aliases.zsh
@@ -1,37 +1,4 @@
 # Bunch of shit from oh-my-zsh. I don't want to handle submoduling that shit
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
-
-alias -- -='cd -'
-alias 1='cd -1'
-alias 2='cd -2'
-alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
-
-alias md='mkdir -p'
-alias rd=rmdir
-
-function d () {
-  if [[ -n $1 ]]; then
-    dirs "$@"
-  else
-    dirs -v | head -n 10
-  fi
-}
-compdef _dirs d
-
-# List directory contents
-alias lsa='ls -lah'
-alias l='ls -lah'
-alias ll='ls -lh'
-alias la='ls -lAh'
 
 alias-finder() {
   local cmd="" exact="" longer="" wordStart="" wordEnd="" multiWordEnd=""
@@ -97,11 +64,12 @@ function gcdnv() {
 	fi
 }
 
-# alias '-'='cd -'
-# alias '...'='../..'
-# alias '....'='../../..'
-# alias '.....'='../../../..'
-# alias '......'='../../../../..'
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+alias -- -='cd -'
 alias 1='cd -1'
 alias 2='cd -2'
 alias 3='cd -3'
@@ -111,7 +79,24 @@ alias 6='cd -6'
 alias 7='cd -7'
 alias 8='cd -8'
 alias 9='cd -9'
-alias _='sudo '
+
+alias md='mkdir -p'
+alias rd=rmdir
+
+function d () {
+  if [[ -n $1 ]]; then
+    dirs "$@"
+  else
+    dirs -v | head -n 10
+  fi
+}
+compdef _dirs d
+
+# List directory contents
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
 alias dbl='docker build'
 alias dcb='docker-compose build'
 alias dcdn='docker-compose down'
